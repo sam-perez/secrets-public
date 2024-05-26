ed9e9d4eda30f4f4b3c0cdefff9bf65e50e72e0a
sam-perez <samuel.e.perez@gmail.com>
2024-05-26 10:50:05 -0700

    Quick hack to fix hydrating issue.
    Seems to be related to our UI library.
    
    The goal here is to have no errors in our console in production. Let's just render nothing on the server and have all rendering happen on the client side.

--- app/routes/_marketing.tsx
+++ app/routes/_marketing.tsx
@@ -1,8 +1,19 @@
+import { useEffect, useState } from "react";
 import { Outlet } from "@remix-run/react";
 import Footer from "~/components/ui/footer";
 import MarketingNav from "~/components/ui/marketing-nav";
 
 export default function MarketingLayout() {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
+  if (!isClient) {
+    return null;
+  }
+
   return (
     <>
       <MarketingNav />
